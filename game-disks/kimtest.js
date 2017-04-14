25da1117a294cbd52283e09d87c1047f94f9d644
lumosNox9 <justabeagle@gmail.com>
2017-04-14 16:51:47 -0500

    Update kimtest.js

--- game-disks/kimtest.js
+++ game-disks/kimtest.js
@@ -1,37 +1,37 @@
- const kimtest = {
-    		roomId: 'it',
-    		inventory: [],
-    		rooms: [
+const  kimtest = {
+			roomId: 'it',
+			inventory: [],
+			rooms: [
 			  {
 			  	name: 'IT',
 			  	id: 'it',
 			  	img: ' ',
 			  	desc: 'You are in IT. You see several desks and computers, a large white board wall, and a broken window. You see exits to the north, east, and west.',
-    			items: [
-    	{ name: 'window', desc: 'This window is cracked from the inside, as though an angry programmer shot it.' },
-    	{ name: 'white board', desc: 'The white board is covered in illegible scrawls. You can make out a few words: cloud, loop, and a lot of question marks.' }
-    	],
-    	exits: [
-		  { dir: 'west', id: 'roland' },        
-		  { dir: 'north', id: 'hallway' },
-		  { dir: 'east', id: 'elevator' }
-    	]
-    	},
+			  	items: [
+					{ name: 'white board', desc: 'The white board is covered in illegible scrawls. You can make out a few words: cloud, loop, and a lot of question marks.' },
+					{ name: 'window', desc: 'This window is cracked from the inside, as though an angry programmer shot it.' }
+			  	],
+			  	exits: [
+				  { dir: 'west', id: 'roland' },
+				  { dir: 'north', id: 'hallway' },
+				  { dir: 'east', id: 'elevator' }
+			  	]
+			  },
     	{
     		name: 'Roland\'s Office',
     		id: 'roland',
     		img: ' ',
     		desc: ' I don\'t know how you got here, but you definitely don\'t belong here.   ',
-    		items: [ ]
+    		items: []
     	},
     	{
-    	name: 'Freight Elevator',
+    		name: 'Freight Elevator',
     		id: 'elevator',
-    	img: '  ',
-    	desc: ' Wow it\'s loud here. ',
-    	items: [
-        
-    	]
+    		img: '  ',
+    		desc: ' Wow it\'s loud here. ',
+    		items: [
+
+    		]
     	},
     	{
     		name: 'Hallway Outside IT',
@@ -39,5 +39,5 @@
     		img: '',
     		desc: ' You hear someone yelling.  '
     	}
-    	]
-    	};
+			]
+		};
